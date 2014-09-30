0f33d3a9c766131e8d21b1fd7d1792bf9f140cce
paw <pawsen@gmail.com>
2014-09-30 21:23:20 +0200

    Changed AJAX url so https works Changed the way date & time is shown

--- js/controller.js
+++ js/controller.js
@@ -1,21 +1,52 @@
+// -*- coding: utf-8 -*-
+
 angular.module('toiletFilter', []).
 filter('timeFormat', function() {
 	return function(input) {
-	  return input > 5000 ? '\u2713' : '\u2718';
-	};
+
+      // function addZ(n) {
+      //   return (n<10? '0':'') + n;
+      // }
+
+      secs=Math.floor(input/1000)%60;
+      mins=Math.floor(input/(1000*60))%60;
+      hrs=Math.floor(input/(1000*60*60))%24;
+      var timeStr = '';
+      if (hrs > 0)
+        timeStr = hrs + '.' + mins + ' t';
+      else if (mins > 0)
+        timeStr = mins + '.' + secs + ' min';
+      else
+        timeStr = secs + ' sek';
+      return timeStr;
+
+      //return (hrs) + ':' + (mins) + ':' + (secs);
+    };
 }).
 filter('dateFormat', function() {
-	return function(input) {
-	  return input > 5000 ? '\u2713' : '\u2718';
-	};
+  return function(input) {
+    timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
+    var date = new Date(input - timezoneOffset);
+    // return date.getDate() + '.' + date.getMonth() + ' ' +
+    return  (date.getHours() - 2) + ':' + date.getMinutes();
+  };
 });
 
 
 var toiletApp = angular.module('toiletApp', ['toiletFilter']);
 
+var loki = false;
+var loki = true;
+if (loki == true){
+  str_url = '/ajax/';
+}else{
+  str_url = 'http://toilet/ajax/';
+}
+
+
 toiletApp.controller('ToiletController', ['$scope', '$http',
 	function ($scope, $http) {
-		$http.get('http://toilet/ajax/occupied').success(function(data) {
+		$http.get(str_url + 'occupied').success(function(data) {
 		$scope.toilets = {};
 		$scope.showers = {};
 		angular.forEach(data, function(value, key) {
@@ -32,7 +63,7 @@ toiletApp.controller('ToiletController', ['$scope', '$http',
 
 function ajaxListener(id, $scope, $http){
 	console.log(id);
-	$http({method: 'GET', url: 'http://toilet/ajax/dump&id='+id}).
+	$http({method: 'GET', url: str_url + 'dump&id='+id}).
 		success(function(data, status, headers, config) {
 			$scope.toilets[data.id] = data;
 			ajaxListener(data.id, $scope, $http);
